5634d83a633c9c1ad85ac2efe3ea981490a6a966
varshini <dreamvarshini@gmail.com>
2021-09-01 10:46:36 +0530

    ADDED:getter and setters for mapnode

---
+++ src/com/bridgelabz/hashtables/HashMapMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hashtables;
+
+
+public class HashMapMain {
+
+	public static void main(String[] args) 
+	
+	{
+		
+
+	}
+
+}
---
+++ src/com/bridgelabz/hashtables/MyMapNode.java
@@ -0,0 +1,60 @@
+package com.bridgelabz.hashtables;
+import com.bridgelabz.linkedlist.*;
+
+
+public class MyMapNode<K,V> implements INodeIF<K>
+{
+	K key;
+	V value;
+	MyMapNode<K, V> next;
+	
+	
+	public MyMapNode(K key,V value) 
+	{
+		this.key=key;
+		this.value=value;
+		next=null;
+		
+	}
+
+	@Override
+	public K getKey() 
+	{
+		
+		return key;
+	}
+
+	@Override
+	public void setKey(K key) 
+	{
+		this.key=key;
+		
+	}
+
+	@Override
+	public INodeIF<K> getNext() 
+	{
+		return next;
+	}
+
+	@Override
+	public void setNext(INodeIF<K> next) 
+	{
+		this.next=(MyMapNode<K, V>) next;
+		
+	}
+	
+	public V getValue()
+	{
+		return value;
+	}
+	
+	public void setValue(V value)
+	{
+		
+		this.value=value;
+	}
+	
+	
+
+}
