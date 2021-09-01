36d3896f517ba93965b53701d39f41ccc370cd3c
varshini <dreamvarshini@gmail.com>
2021-09-01 13:26:17 +0530

    ADDED:function to get node of given key value of linkedhashMap

---
+++ src/com/bridgelabz/hashtables/MyLinkedHashMap.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.hashtables;
+
+import java.util.ArrayList;
+
+import com.bridgelabz.linkedlist.MyLinkedList;
+
+public class MyLinkedHashMap<K,V> 
+{
+	private final int numBuckets;
+	ArrayList<MyLinkedList<K>> myBucketArray;
+	
+	public MyLinkedHashMap() 
+	
+	{
+		this.numBuckets=10;
+		this.myBucketArray=new ArrayList<>(numBuckets);
+		
+		for(int index=0;index<numBuckets;index++)
+		{
+			
+			this.myBucketArray.add(null);
+		}
+	}
+	
+	public V get(K key)
+	{
+		int index=this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if(myLinkedList==null) return null;
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+		return (myMapNode==null)? null : myMapNode.getValue();
+		
+	}
+
+	
+
+}
